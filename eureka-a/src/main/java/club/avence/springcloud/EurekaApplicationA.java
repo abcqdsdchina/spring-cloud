12618ba1e6cdff8bae51bf307c296d5dc8c2b8f6
暮雨落星辰 <abcqdsdchina@126.com>
2019-04-05 15:00:24 +0800

    注册中心配置代码提交

--- eureka-a/src/main/java/club/avence/springcloud/EurekaApplicationA.java
+++ eureka-a/src/main/java/club/avence/springcloud/EurekaApplicationA.java
@@ -2,12 +2,14 @@ package club.avence.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
-public class Application {
+public class EurekaApplicationA {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(EurekaApplicationA.class, args);
     }
 
 }
