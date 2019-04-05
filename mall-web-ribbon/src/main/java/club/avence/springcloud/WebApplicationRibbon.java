60be43d655756fdfb398f5126102b166a410ad74
暮雨落星辰 <abcqdsdchina@126.com>
2019-04-05 21:16:21 +0800

    Ribbon配置代码提交

---
+++ mall-web-ribbon/src/main/java/club/avence/springcloud/WebApplicationRibbon.java
@@ -0,0 +1,32 @@
+package club.avence.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.client.RestTemplate;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+@SpringBootApplication
+public class WebApplicationRibbon {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @Bean
+    public HttpHeaders httpHeaders() {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.set("Authorization",
+                "Basic " + new String(Base64.getEncoder().encode("admin:admin".getBytes(StandardCharsets.US_ASCII))));
+        return httpHeaders;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplicationRibbon.class, args);
+    }
+
+}
---
+++ mall-web-ribbon/src/main/java/club/avence/springcloud/controller/ProductController.java
@@ -0,0 +1,43 @@
+package club.avence.springcloud.controller;
+
+import club.avence.springcloud.product.Product;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+import java.net.URI;
+import java.util.List;
+
+@Slf4j
+@RestController
+public class ProductController {
+
+    private static final String PRODUCTS_TOPIC = "MALL-PRODUCT";
+
+    @Resource
+    private RestTemplate restTemplate;
+
+    @Resource
+    private HttpHeaders httpHeaders;
+
+    @Resource
+    private LoadBalancerClient loadBalancerClient;
+
+    @GetMapping("/products")
+    public List<Product> list() {
+        ServiceInstance serviceInstance = loadBalancerClient.choose(PRODUCTS_TOPIC);
+        URI uri = URI.create(String.format("http://%s:%s/products",
+                serviceInstance.getHost(), serviceInstance.getPort()));
+        return restTemplate.exchange(uri, HttpMethod.GET,
+                    new HttpEntity<>(httpHeaders), List.class)
+                .getBody();
+    }
+
+}
