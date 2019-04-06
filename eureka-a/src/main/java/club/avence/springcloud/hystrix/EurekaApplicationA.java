796a49d12dd94adeb5954782b65471b4df26f46c
暮雨落星辰 <abcqdsdchina@126.com>
2019-04-06 23:01:27 +0800

    Hystrix控制面板代码上传

--- eureka-a/src/main/java/club/avence/springcloud/hystrix/EurekaApplicationA.java
+++ eureka-a/src/main/java/club/avence/springcloud/hystrix/EurekaApplicationA.java
@@ -1,4 +1,4 @@
-package club.avence.springcloud.eureka;
+package club.avence.springcloud.hystrix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class EurekaApplicationB {
+public class EurekaApplicationA {
 
     public static void main(String[] args) {
-        SpringApplication.run(EurekaApplicationB.class, args);
+        SpringApplication.run(EurekaApplicationA.class, args);
     }
 
 }
--- eureka-a/src/main/java/club/avence/springcloud/hystrix/SecurityConfig.java
+++ eureka-a/src/main/java/club/avence/springcloud/hystrix/SecurityConfig.java
@@ -1,4 +1,4 @@
-package club.avence.springcloud.eureka;
+package club.avence.springcloud.hystrix;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
