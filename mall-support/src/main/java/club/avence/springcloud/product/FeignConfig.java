bda8048176992c0fe6896fb72548e37a9f4be8bb
暮雨落星辰 <abcqdsdchina@126.com>
2019-04-06 19:05:02 +0800

    Feign使用代码上传

--- mall-support/src/main/java/club/avence/springcloud/product/FeignConfig.java
+++ mall-support/src/main/java/club/avence/springcloud/product/FeignConfig.java
@@ -1,5 +1,6 @@
 package club.avence.springcloud.product;
 
+import feign.Logger;
 import feign.auth.BasicAuthRequestInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +11,11 @@ public class FeignConfig {
     @Bean
     public BasicAuthRequestInterceptor authRequestInterceptor() {
         return new BasicAuthRequestInterceptor("admin", "admin");
+    }
+
+    @Bean
+    public Logger.Level loggerLevel() {
+        return Logger.Level.FULL;
     }
 
 }
