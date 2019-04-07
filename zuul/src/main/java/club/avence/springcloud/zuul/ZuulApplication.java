a7b1b083bd0d683e9918b761cadbe3595c777b01
暮雨落星辰 <abcqdsdchina@126.com>
2019-04-07 19:29:43 +0800

    Zuul网关配置代码提交

--- zuul/src/main/java/club/avence/springcloud/zuul/ZuulApplication.java
+++ zuul/src/main/java/club/avence/springcloud/zuul/ZuulApplication.java
@@ -1,12 +1,19 @@
 package club.avence.springcloud.hystrix;
 
+import club.avence.springcloud.hystrix.filter.AuthenticationFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @EnableZuulProxy
 @SpringBootApplication
 public class ZuulApplication {
+
+    @Bean
+    public AuthenticationFilter authorizationFilter() {
+        return new AuthenticationFilter();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ZuulApplication.class, args);
---
+++ zuul/src/main/java/club/avence/springcloud/zuul/filter/AuthenticationFilter.java
@@ -0,0 +1,36 @@
+package club.avence.springcloud.hystrix.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+public class AuthenticationFilter extends ZuulFilter {
+
+    @Override
+    public String filterType() {
+        return FilterConstants.PRE_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext context = RequestContext.getCurrentContext();
+        context.addZuulRequestHeader("Authorization",
+                "Basic " + new String(Base64.getEncoder().encode("admin:admin".getBytes(StandardCharsets.US_ASCII))));
+        return null;
+    }
+
+}
