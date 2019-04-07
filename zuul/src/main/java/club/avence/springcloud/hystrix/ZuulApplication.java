54ae9d2ddf3fc3d8c6e39e2cbabd3bdff4f95703
暮雨落星辰 <abcqdsdchina@126.com>
2019-04-07 16:30:07 +0800

    Zuul网关配置

---
+++ zuul/src/main/java/club/avence/springcloud/hystrix/ZuulApplication.java
@@ -0,0 +1,15 @@
+package club.avence.springcloud.hystrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@SpringBootApplication
+public class ZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication.class, args);
+    }
+
+}
