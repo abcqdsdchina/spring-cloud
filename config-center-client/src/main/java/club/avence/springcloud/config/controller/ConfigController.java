cc650503847fd2c0dd1d4697c9d2707be69dec1b
暮雨落星辰 <abcqdsdchina@126.com>
2019-04-13 12:58:14 +0800

    配置中心代码上传

---
+++ config-center-client/src/main/java/club/avence/springcloud/config/controller/ConfigController.java
@@ -0,0 +1,18 @@
+package club.avence.springcloud.config.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ConfigController {
+
+    @Value("${spring.application.name}")
+    private String applicationName;
+
+    @GetMapping("/spring/application/name")
+    public String applicationName() {
+        return applicationName;
+    }
+
+}
