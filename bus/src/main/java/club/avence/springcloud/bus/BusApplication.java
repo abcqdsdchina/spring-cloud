daa48153303f61ebcca356af721f575d390ce936
暮雨落星辰 <abcqdsdchina@126.com>
2019-04-14 12:32:27 +0800

    SpringCloudBus配置

---
+++ bus/src/main/java/club/avence/springcloud/bus/BusApplication.java
@@ -0,0 +1,17 @@
+package club.avence.springcloud.bus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableConfigServer
+@EnableEurekaClient
+@SpringBootApplication
+public class BusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BusApplication.class, args);
+    }
+
+}
