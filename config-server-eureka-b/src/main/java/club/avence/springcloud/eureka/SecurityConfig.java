f685d9600b1e6104d14d3e9969d478b182bd9d68
钱程 <abcqdsdchina@126.com>
2019-07-17 22:44:40 +0800

    代码优化

--- config-server-eureka-b/src/main/java/club/avence/springcloud/eureka/SecurityConfig.java
+++ config-server-eureka-b/src/main/java/club/avence/springcloud/eureka/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Override
     public void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
         super.configure(http);
