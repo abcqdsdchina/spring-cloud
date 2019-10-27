076ce2d1f00fc5c794ac740e62361e458a9d0869
钱程 <abcqdsdchina@126.com>
2019-10-27 23:56:59 +0800

    代码优化

--- bus-client/src/main/java/club/avence/springcloud/bus/client/config/ApplicationConfig.java
+++ bus-client/src/main/java/club/avence/springcloud/bus/client/config/ApplicationConfig.java
@@ -5,6 +5,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
+/**
+ * @author qian3
+ */
 @Data
 @Component
 @RefreshScope
