e9dd10c14053d336f7bc43c267c8400ca09a4661
钱程 <abcqdsdchina@126.com>
2019-11-17 17:30:39 +0800

    增加apollo配置中心的配置

--- config-server/src/main/java/club/avence/springcloud/config/ConfigServerApplication.java
+++ config-server/src/main/java/club/avence/springcloud/config/ConfigServerApplication.java
@@ -3,7 +3,6 @@ package club.avence.springcloud.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author qian3
@@ -13,7 +12,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 public class ConfigServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ConfigServerApplication.class, args);
+        SpringApplication.run(ApolloApplication.class, args);
     }
 
 }
