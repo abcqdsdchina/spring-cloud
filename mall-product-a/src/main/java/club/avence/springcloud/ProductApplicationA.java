afd00658a45e2faf3a4babcf7630ee37b6be71e5
qiancheng <abcqdsdchina@126.com>
2020-05-19 19:30:02 +0800

    增加ResourceServer配置

--- mall-product-a/src/main/java/club/avence/springcloud/ProductApplicationA.java
+++ mall-product-a/src/main/java/club/avence/springcloud/ProductApplicationA.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @EnableEurekaClient
 @EnableCircuitBreaker
+@EnableResourceServer
 @SpringBootApplication(scanBasePackages="club.avence.springcloud")
 public class ProductApplicationA {
 
